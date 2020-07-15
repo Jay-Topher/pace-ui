8f553eaeb6e5844d15e280a634e99bf2962e0ad2
Jones Ogolo <jay.topher11@gmail.com>
2020-07-15 16:33:22 +0100

    mounted first section on app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
+import FirstSection from './components/FirstSection/FirstSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FirstSection />
     </div>
   );
 }
